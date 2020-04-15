18140d032675792d46fa3aa736a0017fefc1b155
srgooglo <traveld8@gmail.com>
2020-04-16 01:23:14 +0200

    scripts update

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,93 +1,135 @@
-import React from 'react'
+import React from 'react';
+import {nugget} from './nugget.js'
 import Router from 'next/router'
-import Head from 'next/head'
-import localforage from 'localforage'
 
-import Main from './main.jsx'
-import Listen from './listen.jsx'
-import AP_list from './ap_list.jsx'
-import Setup from './setup.jsx'
+import {PythonShell} from 'python-shell';
+import * as antd from 'antd'
+import * as Icons from '@ant-design/icons'
+import * as core from '../../core'
+import os from 'os-utils'
+import { Layout,Result,Button,message } from 'antd';
+const { Header,Content } = Layout;
 
-import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 import styles from  './main.css'
 
-const { Header, Content } = Layout;
+let _$options = {
+  mode: 'text',
+  encoding: 'utf8',
+  scriptPath: `${process.cwd()}/py_scripts/`,
+};
+
+const _$interfaces =  new PythonShell('interfaces.py', _$options)
+const _$wash_listen = new PythonShell('wash_listen.py', _$options)
 
 
 export default class Home extends React.PureComponent{
-    constructor(props){
-      super(props),
-      this.state={
-        tab: 0
-      }
+  state = {
+    select_visible: false,
+    using: null,
+    loading: true,
+    interfaces: null,
+    default: null
+  }
+
+  toggleSelectAP(){
+    this.setState({ select_visible: !this.state.select_visible })
+  }
+
+  __get = {
+    interfaces: (callback) => {
+      _$interfaces.on('message', function (message) {
+        if (!message) return false
+        return callback(message)
+      });
+    },
+    wire_ap: (callback) => {
+
     }
+  }
+
   
-    nav = {
-     go: (key)=>{
-       if (!key) return false
-       switch (key) {
-          case 'listen':{
-            return this.stage.listen()
-          }
-          case 'main':{
-            return this.stage.main()
-          }
-          case 'ap_list':{
-            return this.stage.AP_list()
-          }
-          default:{
-             return false
-          }
-       }
+  renderInterfaces(){
+    const { Option } = antd.Select;
+    if (this.state.loading) {
+      return <h3>Gathering Interfaces...</h3>
+    }
+    try {
+      const data = this.state.interfaces
+      const handleChange = (value) => {
+        console.log(`selected ${value}`);
+        this.setState({ using: value })
       }
-    }
-
-    stage = {
-      listen: () => {
-        this.setState({ tab: 1 })
-      },
-      main: () => {
-        this.setState({ tab: 0 })
-      },
-      ap_list: () => {
-        this.setStage({ tab: 3 })
-      },
-    }
-
-    renderStage(){
-      switch (this.state.tab) {
-        case 0:{
-          return <Main router={this.nav}  />
-        }
-        case 1:{
-          return <Listen router={this.nav} />
-        }
-        default:
-          return <div>Render error</div>
-      }
-    }
-
-    render(){
-       return(
-           <React.Fragment>
-                <Head>
-                    <title>Network Wizard</title>
-                </Head>
-        
-                <Header style={{ color: '#fff!important', backgroundColor: '#2D2D2D' }}>
-                  <h3 onClick={() => this.stage.main()} style={{ color: '#fff' }}>Network Wizard</h3>
-                </Header>
-
-                <Content>
-                  {this.renderStage()}
-                </Content>
-           </React.Fragment>
-       )
-     }
     
-      
-     
+      const isdefault = (i) => {
+        if(this.state.default == i){
+          return " DEFAULT |"
+        }
+        return null
+      }
+      if (this.state.loading) {
+        return <h2>Loading</h2>
+      }
+      return (
+        <antd.Select style={{ width: 300 }} defaultValue={this.state.using} onChange={handleChange}>
+             {data.map(item => (<Option key={item.interface}>{isdefault(item.interface)} {item.mac} | {item.ip} </Option>))}
+        </antd.Select>
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  componentDidMount(){
+    try {
+      this.__get.interfaces((data)=>{
+        let db = [];
+        const interfc = JSON.parse(data).interfaces
+        const defaint = JSON.parse(data).default_interface
+
+        interfc.forEach(element => {
+          db.push(JSON.parse(element))
+        });
+  
+        console.log(db, defaint)
+        this.setState({ loading: false, interfaces: db, default: defaint, using: defaint })
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+  render(){ 
+    return(
+      <Content style={{ padding: 48, textAlign: 'center', alignContent: 'center' }}>
+        <div className={styles.brand}>
+         <img style={{ width: '200px' }} src={nugget} />
+         <h1>SPACY NUGGET</h1>
+        </div>
+        <div>
+          <Icons.WifiOutlined /> Select a interface <br/>
+          {this.renderInterfaces()}<br/>
+          <Icons.AimOutlined /> Select an AP<br/>
+          <antd.Button onClick={()=> this.toggleSelectAP()}> Select </antd.Button>
+        </div>
+        <br/><br/>
+        <Button onClick={()=> Router.push('/listen')}> Start Listen </Button>
+        <Button onClick={()=> core.app.setup()}> Install Python3 </Button>
+        <br/>
+        {os.platform()}
+        <antd.Drawer
+          title="Select an AP"
+          placement="right"
+          closable={true}
+          onClose={() => this.toggleSelectAP()}
+          visible={this.state.select_visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </antd.Drawer>
+      </Content>
  
-    
+    )
+  }
 }
+
